e1d97ba03633fa10fccfdfd083fbb1bd6093b630
Yahya <yahyatinani@gmail.com>
2020-06-14 09:00:53 +0100

    Add CRUD queries to manipulate the database

--- resources/sql/queries.sql
+++ resources/sql/queries.sql
@@ -1,21 +1,16 @@
--- :name create-user! :! :n
--- :doc creates a new user record
-INSERT INTO users
-(id, first_name, last_name, email, pass)
-VALUES (:id, :first_name, :last_name, :email, :pass)
+-- :name save-message! :! :n
+-- :doc creates a new message using the name, message, and timestamp keys.
+INSERT INTO guestbook
+    (name, message, timestamp)
+VALUES (:name, :message, :timestamp);
 
--- :name update-user! :! :n
--- :doc updates an existing user record
-UPDATE users
-SET first_name = :first_name, last_name = :last_name, email = :email
-WHERE id = :id
+-- :name get-messages :? :*
+-- :doc selects all available messages
+SELECT *
+FROM guestbook;
 
--- :name get-user :? :1
--- :doc retrieves a user record given the id
-SELECT * FROM users
-WHERE id = :id
-
--- :name delete-user! :! :n
--- :doc deletes a user record given the id
-DELETE FROM users
-WHERE id = :id
+-- :name get-message :? :1
+-- :doc retrieves a message by the given id.
+SELECT *
+FROM guestbook
+WHERE id = :id;
