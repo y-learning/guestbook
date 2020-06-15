1b58b572bd4f2248e2ac90ea0b5d6c8816ef6942
Yahya <yahyatinani@gmail.com>
2020-06-15 12:47:46 +0100

    Implement messages endpoints

--- src/clj/guestbook/routes/home.clj
+++ src/clj/guestbook/routes/home.clj
@@ -5,22 +5,26 @@
     [clojure.java.io :as io]
     [guestbook.middleware :as middleware]
     [ring.util.response]
-    [ring.util.http-response :as response]))
+    [ring.util.http-response :as response])
+  (:import (java.util Date)))
 
 (defn home-page [request]
   (layout/render request
                  "home.html"
-                 {:docs (-> "docs/docs.md"
-                            io/resource
-                            slurp)}))
+                 {:messages (db/get-messages)}))
 
 (defn about-page [request]
   (layout/render request "about.html"))
 
+(defn save-message! [{:keys [params]}]
+  (db/save-message!
+    (assoc params :timestamp (Date.)))
+  (response/found "/"))
+
 (defn home-routes []
   [""
-   {:middleware [middleware/wrap-csrf
-                 middleware/wrap-formats]}
+   {:middleware [middleware/wrap-csrf middleware/wrap-formats]}
    ["/" {:get home-page}]
-   ["/about" {:get about-page}]])
+   ["/about" {:get about-page}]
+   ["/message" {:post (fn [request] (save-message! request))}]])
 
