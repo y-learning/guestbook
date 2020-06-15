ec1c90512cc73c5d09ff05886a5e9840f00b46a0
Yahya <yahyatinani@gmail.com>
2020-06-15 15:55:09 +0100

    Implement input validation

--- src/clj/guestbook/routes/home.clj
+++ src/clj/guestbook/routes/home.clj
@@ -5,26 +5,41 @@
     [clojure.java.io :as io]
     [guestbook.middleware :as middleware]
     [ring.util.response]
-    [ring.util.http-response :as response])
+    [ring.util.http-response :as response]
+    [struct.core :as st])
   (:import (java.util Date)))
 
+(defn validate-message [params]
+  (st/validate
+    params
+    [[:name st/required]
+     [:message st/required [st/min-count 10]]]))
+
 (defn home-page [request]
-  (layout/render request
-                 "home.html"
-                 {:messages (db/get-messages)}))
+  (let [{flash :flash} request]
+    (println flash)
+    (layout/render request
+                   "home.html"
+                   (merge {:messages (db/get-messages)}
+                          (select-keys flash [:name :message :errors])))))
 
 (defn about-page [request]
   (layout/render request "about.html"))
 
 (defn save-message! [{:keys [params]}]
-  (db/save-message!
-    (assoc params :timestamp (Date.)))
-  (response/found "/"))
+  (if-let [errors (first (validate-message params))]
+    (-> (response/found "/")
+        (assoc :flash (assoc params :errors errors)))
+    (do
+      (db/save-message! (assoc params :timestamp (Date.)))
+      (response/found "/"))))
 
 (defn home-routes []
   [""
    {:middleware [middleware/wrap-csrf middleware/wrap-formats]}
-   ["/" {:get home-page}]
+   ["/" {:get (fn [request] (home-page request))}]
    ["/about" {:get about-page}]
    ["/message" {:post (fn [request] (save-message! request))}]])
+
+
 
